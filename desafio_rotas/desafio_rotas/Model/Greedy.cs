a1b19d22d9c9ba23f61a8fc5eaa687910a41998c
bruno <bruno.pduarte0@gmail.com>
2023-12-10 12:42:44 -0300

    fix(master): changed 2nd greedy algorithm criteria

--- desafio_rotas/desafio_rotas/Model/Greedy.cs
+++ desafio_rotas/desafio_rotas/Model/Greedy.cs
@@ -20,13 +20,25 @@ public class Greedy(Transporter transporter)
     public ReportResult DistributeRoutes2ndAlgorithm()
     {
         this.reportResult.startTime();
-        var sortedRoutesReverse = this.transporter.routes.OrderByDescending(r => r).ToArray();
+        var sortedRoutesReverse = this.transporter.routes.OrderBy(r => r).ToArray();
+        int currentTruckIndex = 0;
 
         for (int i = 0; i < sortedRoutesReverse.Length; i++)
         {
-            this.transporter.trucks[i % this.transporter.trucks.Count].AddRoute(sortedRoutesReverse[i]);
+            if (this.transporter.trucks[currentTruckIndex].totalRoute + sortedRoutesReverse[i] <= this.transporter.averageTruckRoutes)
+            {
+                this.transporter.trucks[currentTruckIndex].AddRoute(sortedRoutesReverse[i]);
+            }
+            else
+            {
+                if (currentTruckIndex < this.transporter.trucks.Count - 1)
+                    currentTruckIndex++;
+                else currentTruckIndex = 0;
+                this.transporter.trucks[currentTruckIndex].AddRoute(sortedRoutesReverse[i]);
+            }
         }
-  
+
+
         this.reportResult.endTime();
 
         return reportResult;
